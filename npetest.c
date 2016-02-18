c8d2810ae250d6178e7ca3e4332d2ecb3412f20d
lilott8 <lilott8@gmail.com>
2016-02-17 17:39:16 -0800

    Null pointer dereferencing throws exceptions

--- npetest.c
+++ npetest.c
@@ -7,19 +7,32 @@
 int
 main(int argc, char *argv[])
 {
+
+  if(argc != 2) {
+    printf(2, "npetest [even|odd number]\n");
+    exit();
+    return 0;
+  }
+
+  int dec = atoi(argv[1]);
+  
   printf(2, "Running Null Pointer Tests...\n");
 
   int a = 5;
   int *p = (void*)0;
 
+
   printf(2, "Testing valid data\n");
   printf(2, "a is: %d\n", a);
-  if(*p == 0) {
-    printf(2, "p is pointing to 0\n");
-  }
   
   printf(2, "Testing for NPE now...\n");
-  printf(2, "p is: %d\n", &p);
+  if(dec % 2 == 0){
+    printf(2, "testing with a syscall\n");
+    sleep(*p);
+  } else {
+    printf(2, "p is pointing to %d\n", *p);
+  }
+  // should never get here!
   printf(2, "NPE passed\n");
 
   exit();
